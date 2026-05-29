9a4de6a348db1de96cfec20ff3c3c471b2788237
lsjeong826-png <lsjeong826@gmail.com>
2026-05-29 18:45:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ const CONFIG = {
     venue: "베일홀 B2층, 그랜드 모먼트, 부산",
     hall: "부산 남구 대연동 황령대로 401-9",
     address: "❤️신부측 피로연 안내❤️",
-    tel: "26년 07월 18일(토) 오전 11:00 ~ 오후 14:00, 칠보산 예식장",
+    : "26년 07월 18일(토) 오전 11:00 ~ 오후 14:00, 칠보산 예식장",
     mapLinks: {
       kakao: "https://place.map.kakao.com/1277548361",
       naver: "https://naver.me/GoD7OvE4"
